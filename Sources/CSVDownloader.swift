b941fce4d83e394323f7590eda3e5a7b0c3a52f5
Akshay <akshay@fluidtouch.biz>
2022-05-26 15:09:51 +0530

    - Comma Separated changed to Tab-Separator. - Added Destination Path to improve performance.

--- Sources/CSVDownloader.swift
+++ Sources/CSVDownloader.swift
@@ -11,43 +11,45 @@ import Files
 
 class CSVDownloader {
     let csvURL: URL
+    let destination: String
     var didFailed: Bool = false
-    
-    public init(csvURL: URL) {
+
+    public init(csvURL: URL, destination: String) {
         self.csvURL = csvURL
+        self.destination = destination
     }
-    
+
     func downloadCSV() {
-        
-        print("Downloading Localizable file ...".blue)
+
+        print("⬇️ Downloading Localizable file ...".blue)
         var end = false
 
         let task = URLSession.shared.dataTask(with: csvURL) { [weak self] (data, response, error) in
             guard let self = self else { return }
 
             if let data = data, let csvString = String(data: data, encoding: .utf8) {
-                self.parseCSVString(csvString)
+                self.parseCSVString(csvString, destination: self.destination)
             }
-            
+
             end = true
         };
-        
+
         task.resume() // launch task
-        
-        // show ASCII animation while downloading
+
+            // show ASCII animation while downloading
         let animation = ["|", "/", "-", "\\"]
         var animationIndex = 0
         while (!end) {
 
             print("\(animation[animationIndex])\r", separator: "", terminator: "")
-            
+
             if animationIndex < animation.count - 1 {
                 animationIndex = animationIndex + 1
             } else {
                 animationIndex = 0
             }
         }
-        
+
         if didFailed {
             print("❌ ERROR CREATING LOCALIZABLE ❌".red)
             exit(EXIT_FAILURE)
@@ -55,24 +57,24 @@ class CSVDownloader {
             print("✅ LOCALIZABLES FILES DID UPDATE SUCCESSFULLY".lightGreen)
             exit(EXIT_SUCCESS)
         }
-        
+
         print("\r  ")
     }
-    
-    private func parseCSVString(_ csvString: String) {
-        
+
+    private func parseCSVString(_ csvString: String, destination: String) {
+
         var localizables = [Localizable]()
         var rows = csvString.components(separatedBy: "\n")
-        
+
         guard let firstRow = rows.first else {
             return
         }
-        
-        let languages = firstRow.components(separatedBy: ",")
+
+        let languages = firstRow.components(separatedBy: "\t")
             .map { $0.replacingOccurrences(of: "\r", with: "")}
             .filter { !$0.contains("Identifier")}
-        
-        
+
+
         guard languages.count != 0 else {
             print("You need to add at least one language colum on the document ❌".red)
             didFailed.toggle()
@@ -80,32 +82,32 @@ class CSVDownloader {
         }
 
         rows.removeFirst()
-        
+
         var loopCount = 1
         var valueIndex = 2
         while loopCount <= languages.count {
             rows.forEach { row in
-                let colums = row.components(separatedBy: ",")
-                
+                let colums = row.components(separatedBy: "\t")
+
                 let key = colums[0]
                 let value = colums[valueIndex].replacingOccurrences(of: "\r", with: "")
                 localizables.append(.init(key: key, value: value))
-   
+
             }
-            
+
             valueIndex += 1
-            // save in document
+                // save in document
             do {
-                try LocalizableGenerator.createFile(for: languages[loopCount - 1], with: localizables)
+                try LocalizableGenerator.createFile(for: languages[loopCount - 1], with: localizables, destination: destination)
             } catch {
                 print("❌ ERROR CREATING LOCALIZABLE ❌".red)
                 didFailed.toggle()
                 exit(EXIT_FAILURE)
             }
-            
-            // turn empty the localizables array
+
+                // turn empty the localizables array
             localizables.removeAll()
-            
+
             loopCount += 1
         }
     }
--- Sources/GenerateLocalizables.swift
+++ Sources/GenerateLocalizables.swift
@@ -6,22 +6,24 @@ import SwiftFigletKit
 
 @main
 struct GenerateLocalizables: ParsableCommand {
+    @Argument(help: "Enter the PATH where the Files to be created")
+    var destinationPath: String
     @Argument(help: "Enter the URL where the document is hosted")
     var url: String
-    
+
     func run() throws {
-        
-        guard let url = URL(string: url + "/export?format=csv"),
+
+        guard let url = URL(string: url + "/export?format=tsv"),
               self.url.contains("https://") else {
-                  print("❌ Need a valid URL for the document, ".red + "'\( url )' is not a valid URL format".asMagenta)
+            print("❌ Need a valid URL for the document, ".red + "'\( url )' is not a valid URL format".asMagenta)
             return
         }
-        
-        CSVDownloader(csvURL: url).downloadCSV()
-        
+        print("🗂 Destination Folder \(Folder.current.path)/\(destinationPath)")
+        print("🔗 Google Sheets URL \(url.absoluteString)")
+
+        CSVDownloader(csvURL: url, destination: destinationPath).downloadCSV()
+
         RunLoop.main.run()
     }
-    
-    
 }
 
